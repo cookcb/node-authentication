593652a1b5b407a0ed2de23eef03519339ba9494
Cook <ccook@inmar.com>
2018-08-27 15:39:57 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
+
 const app = express()
 
 var PORT = 8080
 
 const port = process.env.PORT || 8080;
-const publicPath = express.static(path.join(__dirname, '../'));
 
-app.use(publicPath);
+const indexPath = path.join(__dirname, '/index.html')
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.sendFile(indexPath);
 })
 
-app.listen(PORT, () => console.log('Example app listening on port 3000!'));
+app.listen(PORT, () => console.log(`Example app listening on port $(PORT)!`));
+
 
