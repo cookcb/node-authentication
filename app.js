40aaa389d6128a7526053f4a9c56cef019d712bf
cbcook <cbcook321@gmail.com>
2018-09-02 23:11:49 -0400

    Added connect-session

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser')
 const passport = require('passport');
+const flash = require('connect-flash');
+
 require('./config/passport')(passport);
 
 const app = express()
@@ -13,18 +15,15 @@ const port = process.env.PORT || 8080;
 const indexPath = path.join(__dirname, '/index.html')
 
 app.use(express.static(path.join(__dirname, '/public')));
-app.use( bodyParser.json() );       
+app.use(bodyParser.json());       
 app.use(bodyParser.urlencoded({     
   extended: true
 })); 
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
 
 app.use('/', routes)
-
-app.get('/', (req, res) => {
-    res.sendFile(indexPath);
-})
 
 app.listen(port, () => console.log('Example app listening on port ' + port));
 
