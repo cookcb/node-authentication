4a51a101ed91cc3fc5cbe82ba99f2774a7d66721
cbcook <cbcook321@gmail.com>
2018-09-08 17:58:58 -0400

    Added form and on change handler to register.js

--- src/components/Register.js
+++ src/components/Register.js
@@ -3,13 +3,42 @@ import React, {Component} from 'react';
 class Register extends Component{
     constructor(props){
         super(props);
+        this.state = {
+            username: "",
+            password: ""
+        }
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event){
+        let name = event.target.name;
+        let value = event.target.value;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit(event){
+        
     }
 
     render() {
         return (
-            <div>
-                Success!
-            </div>
+            <form onSubmit={this.handleSubmit}>
+                <div>
+                    <label for="username">Enter Username:</label>
+                    <input name="username" type="text" onChange={this.handleChange}/>
+                </div>
+                <div>
+                    <label for="password">Enter Password:</label>
+                    <input name="password" type="text" onChange={this.handleChange}/>
+                </div>
+                <div>
+                    <input type="submit" value="Register" />
+                </div>
+            </form>
         )
     }
 }
